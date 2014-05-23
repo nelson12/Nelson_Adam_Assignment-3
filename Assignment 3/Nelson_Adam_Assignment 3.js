5b425286a96fe470db6e6f1849fd3579959383cd
nelson12# <aw.nelson@hotmail.com>
2014-05-22 21:24:59 -0400

    variables and output

---
+++ Assignment 3/Nelson_Adam_Assignment 3.js
@@ -0,0 +1,24 @@
+//Adam Nelson
+//SDI Online
+//05/22/2014
+//Project 3
+
+//Declaring variables
+var game= "World of Warcraft";
+var races= 14;
+var classes=11;
+var loggingIn="Password";
+var yourRace= "Human"; 
+var yourClass= "Hunter";
+var start= 'Let\’s play!';
+var hunter= true;
+var petNames = [ "Lucy" , "Snowball" , "Frosty" ];
+
+//Output
+console.log(" You are playing " + game + ".");
+console.log(" There are " + races + " races. ");
+console.log(" There are " + classes + " classes. ");
+console.log(" You will be using your " + loggingIn + " to authenticate. ");
+console.log(" You will be a " + yourRace +  yourClass + ".");
+console.log(" Come on " + start + "!"); 
+
